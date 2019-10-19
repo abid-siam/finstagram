25b1d6324f97dc28f54be06f30ea4a9e6593a944
Abid Siam <as10476@nyu.edu>
2019-10-19 02:13:17 -0400

    Final

--- connection.py
+++ connection.py
@@ -3,6 +3,6 @@ import pymysql.cursors
 conn = pymysql.connect(host='localhost',
                        user='root',
                        password='',
-                       db='Finstagram',
+                       db='finstagram',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
